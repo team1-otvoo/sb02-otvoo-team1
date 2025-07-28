e6e6f014548227b775ac35a95cf10db550073b7b
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-28 18:32:09 +0900

    chore: KAN-16 PR 템플릿 추가
    * feat: KAN-15 기본 예외 처리 구현
    
    * chore: KAN-16 PR 템플릿 추가

---
+++ src/main/java/com/team1/otvoo/OtvooApplication.java
@@ -0,0 +1,13 @@
+package com.team1.otvoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtvooApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OtvooApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/team1/otvoo/OtvooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.otvoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtvooApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
