d503f78bc5e51229d5de461a9146501f0e03f5d0
nayu-yuna <dldldbsk@naver.com>
2025-07-30 14:04:32 +0900

    feat: KAN-38 DM

---
+++ src/main/java/com/team1/otvoo/directmessage/dto/DirectMessageCreateRequest.java
@@ -0,0 +1,10 @@
+package com.team1.otvoo.directmessage.dto;
+
+import java.util.UUID;
+
+public record DirectMessageCreateRequest(
+        UUID senderId,
+        UUID receiverId,
+        String content
+) {
+}
