46f339eaa8fa330522b5cecd9003bf23c201e8a7
Leekb0804 <lkb8713@gmail.com>
2025-07-31 17:18:48 +0900

    feat: KAN-103 비밀번로 변경 api 추가

--- src/main/java/com/team1/otvoo/user/controller/UserController.java
+++ src/main/java/com/team1/otvoo/user/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.team1.otvoo.user.controller;
 
+import com.team1.otvoo.user.dto.ChangePasswordRequest;
 import com.team1.otvoo.user.dto.UserCreateRequest;
 import com.team1.otvoo.user.dto.UserDto;
 import com.team1.otvoo.user.service.UserService;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +30,15 @@ public class UserController {
     return ResponseEntity
         .status(HttpStatus.CREATED)
         .body(userDto);
+  }
+
+  @PatchMapping("/{userId}/password")
+  ResponseEntity<Void> changePassword(
+      @PathVariable UUID userId,
+      @Valid @RequestBody ChangePasswordRequest changePasswordRequest
+  ) {
+    userService.changePassword(userId, changePasswordRequest);
+    return ResponseEntity.noContent().build();
   }
 
 
