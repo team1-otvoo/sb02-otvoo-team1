130cf3c7f2ca9cfc6544461978c2a77c75da4af9
JaeKwan AN <jaekwan99@gmail.com>
2025-07-31 14:38:44 +0900

    feat: KAN-92 엔티티 생성

---
+++ src/main/java/com/team1/otvoo/recommendation/entity/RecommendationClothes.java
@@ -0,0 +1,47 @@
+package com.team1.otvoo.recommendation.entity;
+
+import com.team1.otvoo.clothes.entity.Clothes;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "recommendation_clothes")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RecommendationClothes {
+
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "recommendation_id", nullable = false)
+  private Recommendation recommendation;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "clothes_id", nullable = false)
+  private Clothes clothes;
+
+  @Column(name = "clothes_order", nullable = false)
+  private int clothesOrder;
+
+  public RecommendationClothes(Clothes clothes, int clothesOrder) {
+    this.clothes = clothes;
+    this.clothesOrder = clothesOrder;
+  }
+
+  public void setRecommendation(Recommendation recommendation) {
+    this.recommendation = recommendation;
+  }
+
+}
