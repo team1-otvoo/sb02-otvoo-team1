4cfb7fa12bb7e49becc89e929d45a9ac79097a1b
Leekb0804 <lkb8713@gmail.com>
2025-07-31 13:08:27 +0900

    feat: KAN-84 UserRepository 구현

---
+++ src/main/java/com/team1/otvoo/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.team1.otvoo.user.repository;
+
+import com.team1.otvoo.user.entity.User;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+  boolean existsByEmail(String email);
+}
