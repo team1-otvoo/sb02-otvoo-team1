7ae6b28d91fa558785f745320fb63a4ef02407b5
JaeKwan AN <jaekwan99@gmail.com>
2025-07-31 16:11:59 +0900

    feat: KAN-99 UUID 생성 전략 추가

--- src/main/java/com/team1/otvoo/recommendation/entity/RecommendationClothes.java
+++ src/main/java/com/team1/otvoo/recommendation/entity/RecommendationClothes.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -21,7 +22,7 @@ import lombok.NoArgsConstructor;
 public class RecommendationClothes {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
   @ManyToOne(fetch = FetchType.LAZY)
