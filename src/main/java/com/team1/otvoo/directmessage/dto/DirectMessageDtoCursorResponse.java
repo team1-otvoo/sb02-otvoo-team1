2c77ab73fc706e0e2e406de37f95d4281a2a1c34
nayu-yuna <dldldbsk@naver.com>
2025-07-31 14:37:04 +0900

    refactor: KAN-88 entity 및 Dto 수정 및 구조 정리

--- src/main/java/com/team1/otvoo/directmessage/dto/DirectMessageDtoCursorResponse.java
+++ src/main/java/com/team1/otvoo/directmessage/dto/DirectMessageDtoCursorResponse.java
@@ -4,12 +4,13 @@ import java.util.List;
 import java.util.UUID;
 
 public record DirectMessageDtoCursorResponse(
-        List<DirectMessageDto> data,
-        String nextCursor,
-        UUID nextIdAfter,
-        boolean hasNext,
-        int totalCount,
-        String sortBy,
-        String sortDirection
+
+    List<DirectMessageDto> data,
+    String nextCursor,
+    UUID nextIdAfter,
+    boolean hasNext,
+    int totalCount,
+    String sortBy,
+    String sortDirection
 ) {
 }
