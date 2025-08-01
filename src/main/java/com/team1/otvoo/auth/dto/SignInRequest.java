97d63fb475649d4255b6158e49e5c385ba147036
nayu-yuna <dldldbsk@naver.com>
2025-08-01 16:05:12 +0900

    feat: KAN-147 로그인 DTO 생성

---
+++ src/main/java/com/team1/otvoo/auth/dto/SignInRequest.java
@@ -0,0 +1,7 @@
+package com.team1.otvoo.auth.dto;
+
+public record SignInRequest(
+    String email,
+    String password
+) {
+}
---
+++ src/main/java/com/team1/otvoo/auth/dto/SignInResponse.java
@@ -0,0 +1,7 @@
+package com.team1.otvoo.auth.dto;
+
+public record SignInResponse(
+    String accessToken,
+    String refreshToken
+) {
+}
