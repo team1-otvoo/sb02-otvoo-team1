98d09136983cc6ce8aeb302cedde9408cca4619d
nayu-yuna <dldldbsk@naver.com>
2025-08-01 17:27:03 +0900

    feat: KAN-151 /api/auth/sign-in 컨트롤러 추가

--- src/main/java/com/team1/otvoo/auth/controller/AuthController.java
+++ src/main/java/com/team1/otvoo/auth/controller/AuthController.java
@@ -1,15 +1,15 @@
 package com.team1.otvoo.auth.controller;
 
 import com.team1.otvoo.auth.dto.CsrfTokenResponse;
+import com.team1.otvoo.auth.dto.SignInRequest;
+import com.team1.otvoo.auth.dto.SignInResponse;
 import com.team1.otvoo.auth.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -26,5 +26,14 @@ public class AuthController {
     log.info("✅ CSRF 토큰 API 호출 성공");
 
     return ResponseEntity.status(HttpStatus.OK).body(csrfToken);
+  }
+
+  @PostMapping("/sign-in")
+  public ResponseEntity<SignInResponse> signIn(@RequestBody SignInRequest request) {
+    log.info("🔷 로그인 시도: email={}", request.email());
+    SignInResponse response = authService.signIn(request);
+
+    log.info("✅ 로그인 완료: accessToken={}, refreshToken={}", response.accessToken(), response.refreshToken());
+    return ResponseEntity.ok(response);
   }
 }
