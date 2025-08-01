43386f2c9b2779f1ca9acd8877ae750a0473b53b
hyorim <tg305@naver.com>
2025-08-01 17:12:11 +0900

    feat: KAN-156 ClothesAttributeDefMapper 및 Repository 생성

---
+++ src/main/java/com/team1/otvoo/clothes/repository/ClothesAttributeDefRepository.java
@@ -0,0 +1,10 @@
+package com.team1.otvoo.clothes.repository;
+
+import com.team1.otvoo.clothes.entity.ClothesAttributeDefinition;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClothesAttributeDefRepository extends JpaRepository<ClothesAttributeDefinition,UUID> {
+
+  boolean existsByName(String name);
+}
