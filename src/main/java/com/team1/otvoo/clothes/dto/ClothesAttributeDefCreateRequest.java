02be85c4bc038e545a4b51482fbb4b55b5f311b2
hyorim <tg305@naver.com>
2025-08-02 16:41:11 +0900

    feat: KAN-183 ClothesAttributeDef Validation 개선

--- src/main/java/com/team1/otvoo/clothes/dto/ClothesAttributeDefCreateRequest.java
+++ src/main/java/com/team1/otvoo/clothes/dto/ClothesAttributeDefCreateRequest.java
@@ -1,14 +1,13 @@
 package com.team1.otvoo.clothes.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 
 public record ClothesAttributeDefCreateRequest(
     @NotBlank(message = "의상 속성 이름은 필수입니다.")
     String name,
 
-    List<@NotEmpty(message = "공백은 입력할 수 없습니다.") String> selectableValues
+    List<@NotBlank(message = "공백은 입력할 수 없습니다.") String> selectableValues
 ) {
 
 }
--- src/main/java/com/team1/otvoo/clothes/dto/ClothesAttributeDefUpdateRequest.java
+++ src/main/java/com/team1/otvoo/clothes/dto/ClothesAttributeDefUpdateRequest.java
@@ -1,10 +1,12 @@
 package com.team1.otvoo.clothes.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 
 public record ClothesAttributeDefUpdateRequest(
+    @NotBlank(message = "의상 속성 이름은 필수입니다.")
     String name,
-    List<String> selectableValues
+    List<@NotBlank(message = "공백은 입력할 수 없습니다.") String> selectableValues
 ) {
 
 }
