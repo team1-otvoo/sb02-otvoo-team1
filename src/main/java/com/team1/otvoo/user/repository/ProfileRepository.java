4f6de9964e649c0287cd4033816240aa6e36b133
Leekb0804 <lkb8713@gmail.com>
2025-08-05 14:31:42 +0900

    feat: KAN-215 Profile findByUserId 메서드, profileImageRepository 구현

--- src/main/java/com/team1/otvoo/user/repository/ProfileRepository.java
+++ src/main/java/com/team1/otvoo/user/repository/ProfileRepository.java
@@ -3,6 +3,7 @@ package com.team1.otvoo.user.repository;
 import com.team1.otvoo.user.entity.Profile;
 import com.team1.otvoo.user.projection.UserNameView;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +18,6 @@ public interface ProfileRepository extends JpaRepository<Profile, UUID> {
     WHERE p.user.id IN :userIds
 """)
   List<UserNameView> findUserNamesByUserIds(@Param("userIds") List<UUID> userIds);
+
+  Optional<Profile> findByUserId(UUID userId);
 }
