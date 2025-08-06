15373919c7219688c116c150122f1f82a1d555b2
Leekb0804 <lkb8713@gmail.com>
2025-08-06 14:31:21 +0900

    feat: KAN-231 프로필 업데이트 비즈니스 로직 코드 추가

--- src/main/java/com/team1/otvoo/user/service/UserService.java
+++ src/main/java/com/team1/otvoo/user/service/UserService.java
@@ -2,17 +2,20 @@ package com.team1.otvoo.user.service;
 
 import com.team1.otvoo.user.dto.ChangePasswordRequest;
 import com.team1.otvoo.user.dto.ProfileDto;
+import com.team1.otvoo.user.dto.ProfileUpdateRequest;
 import com.team1.otvoo.user.dto.UserCreateRequest;
 import com.team1.otvoo.user.dto.UserDto;
 import com.team1.otvoo.user.dto.UserDtoCursorRequest;
 import com.team1.otvoo.user.dto.UserDtoCursorResponse;
 import java.util.UUID;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
 
   UserDtoCursorResponse getUsers(UserDtoCursorRequest request);
   UserDto createUser(UserCreateRequest userCreateRequest);
   ProfileDto getUserProfile(UUID userId);
+  ProfileDto updateProfile(UUID userId, ProfileUpdateRequest profileUpdateRequest, MultipartFile profileImageFile);
   void changePassword(UUID userId, ChangePasswordRequest request);
 
 }
