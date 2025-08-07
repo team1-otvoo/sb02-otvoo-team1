8fa883c7b6dffe18d3d1d33e8f027c0e6ed690df
hyorim <tg305@naver.com>
2025-08-07 11:04:50 +0900

    feat: KAN-247 목록 조회용 SortBy, SortDirection enum 정의

---
+++ src/main/java/com/team1/otvoo/clothes/dto/SortDirection.java
@@ -0,0 +1,33 @@
+package com.team1.otvoo.clothes.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.team1.otvoo.exception.ErrorCode;
+import com.team1.otvoo.exception.RestException;
+import java.util.Map;
+
+public enum SortDirection {
+  ASCENDING,
+  DESCENDING;
+
+  @JsonCreator(mode = Mode.DELEGATING)
+  public static SortDirection from(String value) {
+    if (value == null || value.isBlank()) {
+      return ASCENDING;
+    }
+    return switch (value.toLowerCase()) {
+      case "ascending", "asc" -> ASCENDING;
+      case "descending", "desc" -> DESCENDING;
+      default -> throw new RestException(
+          ErrorCode.INVALID_SORT_DIRECTION,
+          Map.of("sortDirection", value)
+      );
+    };
+  }
+
+  @JsonValue
+  public String toValue() {
+    return this.name().toLowerCase();
+  }
+}
