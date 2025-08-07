02c7e7d5634e1fbfb23e614348cc4a0ec73381f4
Leekb0804 <lkb8713@gmail.com>
2025-08-07 15:08:11 +0900

    fix: KAN-257 queryDsl 관련 레포지토리 명 Custom 으로 통일

--- src/main/java/com/team1/otvoo/user/repository/UserRepositoryCustom.java
+++ src/main/java/com/team1/otvoo/user/repository/UserRepositoryCustom.java
@@ -3,6 +3,6 @@ package com.team1.otvoo.user.repository;
 import com.team1.otvoo.user.dto.UserDtoCursorRequest;
 import com.team1.otvoo.user.dto.UserSlice;
 
-public interface UserQueryDslRepository {
+public interface UserRepositoryCustom {
   UserSlice searchUsersWithCursor(UserDtoCursorRequest request);
 }
