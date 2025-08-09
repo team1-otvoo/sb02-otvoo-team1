1739d65a988ab7933cd07d4693857956e3903144
hyorim <tg305@naver.com>
2025-08-09 13:22:26 +0900

    feat: KAN-290 DTO 검증, TYPE 확장, 에러코드 추가

--- src/main/java/com/team1/otvoo/clothes/dto/ClothesCreateRequest.java
+++ src/main/java/com/team1/otvoo/clothes/dto/ClothesCreateRequest.java
@@ -2,13 +2,15 @@ package com.team1.otvoo.clothes.dto;
 
 import com.team1.otvoo.clothes.dto.clothesAttributeDef.ClothesAttributeDto;
 import com.team1.otvoo.clothes.entity.ClothesType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
 public record ClothesCreateRequest(
-    UUID ownerId,
-    String name,
-    ClothesType type,
+    @NotNull UUID ownerId,
+    @NotBlank (message = "의상 이름은 필수입니다") String name,
+    @NotNull ClothesType type,
     List<ClothesAttributeDto> attributes
 ) {
 
