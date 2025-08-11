7d97f163e0e9793b1eba0b384bfaef7f29a42ff5
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-08-11 09:57:52 +0900

    test: KAN-288 댓글 목록 조회 Service 단위테스트 작성

--- src/test/java/com/team1/otvoo/comment/service/CommentServiceTest.java
+++ src/test/java/com/team1/otvoo/comment/service/CommentServiceTest.java
@@ -1,6 +1,7 @@
 package com.team1.otvoo.comment.service;
 
 import com.team1.otvoo.comment.dto.CommentCreateRequest;
+import com.team1.otvoo.comment.dto.CommentCursor;
 import com.team1.otvoo.comment.dto.CommentDto;
 import com.team1.otvoo.comment.entity.FeedComment;
 import com.team1.otvoo.comment.mapper.CommentMapper;
@@ -11,6 +12,8 @@ import com.team1.otvoo.feed.repository.FeedRepository;
 import com.team1.otvoo.user.dto.AuthorDto;
 import com.team1.otvoo.user.entity.User;
 import com.team1.otvoo.user.repository.UserRepository;
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
@@ -19,7 +22,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.testcontainers.shaded.org.checkerframework.checker.units.qual.C;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
@@ -105,5 +111,29 @@ public class CommentServiceTest {
     assertThatThrownBy(() -> commentService.create(request))
         .isInstanceOf(RestException.class)
         .hasMessageContaining("피드가 존재하지 않습니다.");
+  }
+
+  @Test
+  @DisplayName("댓글 목록 조회 성공")
+  void comment_find_with_cursor_success() {
+    // given
+    UUID feedId = UUID.randomUUID();
+    CommentDto commentDto = new CommentDto(
+        UUID.randomUUID(),
+        Instant.now(),
+        feedId,
+        new AuthorDto(UUID.randomUUID(), "test", "test.url"),
+        "testComment");
+    CommentCursor commentCursor = mock(CommentCursor.class);
+
+    Slice<CommentDto> commentDtoSlice = new SliceImpl<>(List.of(commentDto));
+    given(commentRepository.findCommentsWithCursor(any(), any())).willReturn(commentDtoSlice);
+
+    // when
+    Slice<CommentDto> results = commentService.getCommentsWithCursor(commentCursor, feedId);
+
+    // then
+    assertThat(results.getContent().size()).isEqualTo(1);
+    assertThat(results.getContent().get(0).content()).isEqualTo("testComment");
   }
 }
