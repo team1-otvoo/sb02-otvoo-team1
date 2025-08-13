279830c4fd512c714ad26a4bcfb9b2f8374532ab
JaeKwan AN <jaekwan99@gmail.com>
2025-08-14 00:37:07 +0900

    refactor: KAN-349 Forecast 및 Location 엔티티 수정

--- src/main/java/com/team1/otvoo/weather/entity/WeatherForecast.java
+++ src/main/java/com/team1/otvoo/weather/entity/WeatherForecast.java
@@ -5,9 +5,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.Instant;
@@ -42,7 +45,8 @@ public class WeatherForecast {
   @Column(name = "updated_at", nullable = false)
   private Instant updatedAt;
 
-  @OneToOne(mappedBy = "forecast", cascade = CascadeType.ALL)
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "weather_location_id", nullable = false)
   private WeatherLocation location;
 
   @OneToOne(mappedBy = "forecast", cascade = CascadeType.ALL)
