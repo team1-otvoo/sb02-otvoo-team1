54f58d8fea3ba7489671479bdd31b4805c03de07
JaeKwan AN <jaekwan99@gmail.com>
2025-08-15 15:47:51 +0900

    feat: KAN-357 배치 Job/Step 구조 정의

--- src/main/java/com/team1/otvoo/config/BatchConfig.java
+++ src/main/java/com/team1/otvoo/config/BatchConfig.java
@@ -3,16 +3,17 @@ package com.team1.otvoo.config;
 import com.team1.otvoo.weather.batch.WeatherForecastProcessor;
 import com.team1.otvoo.weather.batch.WeatherItemReader;
 import com.team1.otvoo.weather.batch.WeatherItemWriter;
-import com.team1.otvoo.weather.dto.VilageFcstResponse.FcstItem;
 import com.team1.otvoo.weather.entity.WeatherForecast;
-import java.util.List;
+import com.team1.otvoo.weather.entity.WeatherLocation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.SimpleStepBuilder;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -27,26 +28,39 @@ public class BatchConfig {
   private final WeatherItemReader reader;
   private final WeatherItemWriter writer;
 
-  // Job 정의: weatherJob
+  // Job 정의: weatherBatchJob
   @Bean
-  public Job weatherJob() {
-    return new JobBuilder("weatherJob", jobRepository)
-        .start(weatherStep())  // Step을 연결
+  public Job weatherBatchJob() {
+    return new JobBuilder("weatherBatchJob", jobRepository)
+        .start(weatherForecastStep())  // Step을 연결
         .build();
   }
 
-  // Step 정의: weatherStep
+  // Step 정의: weatherForecastStep
   @Bean
-  public Step weatherStep() {
-    SimpleStepBuilder<List<FcstItem>, List<WeatherForecast>> builder =
-        new StepBuilder("weatherStep", jobRepository)
-            .<List<FcstItem>, List<WeatherForecast>>chunk(1, transactionManager);
-
-    return builder
-        .reader(reader)
-        .processor(processor)
-        .writer(writer)
-        .faultTolerant()
+  public Step weatherForecastStep() {
+    return new StepBuilder("weatherForecastStep", jobRepository)
+        .<WeatherLocation, WeatherForecast>chunk(10, transactionManager)
+        .reader(weatherLocationReader())
+        .processor(weatherForecastProcessor())
+        .writer(weatherForecastWriter())
         .build();
   }
+
+  @Bean
+  public ItemReader<WeatherLocation> weatherLocationReader() {
+    return null;
+  }
+
+  @Bean
+  public ItemProcessor<WeatherLocation, WeatherForecast> weatherForecastProcessor() {
+    return null;
+  }
+
+  @Bean
+  public ItemWriter<WeatherForecast> weatherForecastWriter() {
+    return null;
+  }
+
+
 }
