32b9e8aaf6d09e974234da9597c439d4434d9cd1
JaeKwan AN <jaekwan99@gmail.com>
2025-08-16 00:18:10 +0900

    feat: KAN-359 Processor 구현

--- src/main/java/com/team1/otvoo/weather/repository/WeatherForecastRepository.java
+++ src/main/java/com/team1/otvoo/weather/repository/WeatherForecastRepository.java
@@ -1,7 +1,9 @@
 package com.team1.otvoo.weather.repository;
 
 import com.team1.otvoo.weather.entity.WeatherForecast;
+import com.team1.otvoo.weather.entity.WeatherLocation;
 import com.team1.otvoo.weather.repository.custom.WeatherForecastRepositoryCustom;
+import java.time.Instant;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,5 @@ import org.springframework.stereotype.Repository;
 public interface WeatherForecastRepository extends JpaRepository<WeatherForecast, UUID>,
     WeatherForecastRepositoryCustom {
 
+  boolean existsByLocationAndForecastAt(WeatherLocation location, Instant forecastAt);
 }
