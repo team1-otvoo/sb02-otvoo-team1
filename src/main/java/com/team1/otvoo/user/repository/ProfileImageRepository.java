31d598aa07456ac9c88edec6893e6838555614ef
Leekb0804 <lkb8713@gmail.com>
2025-08-23 09:18:35 +0900

    refactor: KAN-407 사용하지않는 repository 메서드 삭제

--- src/main/java/com/team1/otvoo/user/repository/ProfileImageRepository.java
+++ src/main/java/com/team1/otvoo/user/repository/ProfileImageRepository.java
@@ -4,15 +4,7 @@ import com.team1.otvoo.user.entity.ProfileImage;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ProfileImageRepository extends JpaRepository<ProfileImage, UUID> {
   Optional<ProfileImage> findByProfileId(UUID profileId);
-
-  @Query("""
-    SELECT pi.imageUrl
-    FROM ProfileImage pi
-    WHERE pi.profile.id = :profileId
-""")
-  Optional<String> findUrlByProfileId(UUID profileId);
 }
