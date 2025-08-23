1824bc3731e4d019ad556cab8573860a03ae7bbf
Leekb0804 <lkb8713@gmail.com>
2025-08-23 09:30:47 +0900

    feat: KAN-413 upload 시 url 반환, download 메서드 구현

--- src/main/java/com/team1/otvoo/storage/S3ImageStorage.java
+++ src/main/java/com/team1/otvoo/storage/S3ImageStorage.java
@@ -1,12 +1,15 @@
 package com.team1.otvoo.storage;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public interface S3ImageStorage {
 
-  void upload(String key, InputStream in, long length, String contentType);
+  String upload(String key, InputStream in, long length, String contentType);
 
   void delete(String key);
 
   String getPresignedUrl(String key, String contentType);
+
+  byte[] download(String key) throws IOException;
 }
