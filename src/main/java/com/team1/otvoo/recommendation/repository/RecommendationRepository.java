877957c8b962cedc48fc4bd941c360fe77a00c9c
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-08-28 10:39:32 +0900

    fix: KAN-481 추천 조회 로직 수정 (#105)
    - 첫 추천 조회 시 의상이 null인 경우, 다시 추천받기 버튼을 눌러야만 추천이 되던 문제 해결
    - 추천 조회 시 userId 기반 필터링 기능 추가

--- src/main/java/com/team1/otvoo/recommendation/repository/RecommendationRepository.java
+++ src/main/java/com/team1/otvoo/recommendation/repository/RecommendationRepository.java
@@ -6,6 +6,6 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecommendationRepository extends JpaRepository<Recommendation, UUID> {
-  Optional<Recommendation> findByWeather_Id(UUID weatherId);
+  Optional<Recommendation> findByWeather_IdAndUser_Id(UUID weatherId, UUID userId);
   void deleteAllByWeather_Id(UUID weatherId);
 }
