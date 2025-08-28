e7e23e95eafc3e7f676b38a1bb8737a7c396584f
JaeKwan AN <165141068+kkwan99@users.noreply.github.com>
2025-08-28 16:37:41 +0900

    fix: KAN-510 통합테스트 관련 사항 수정 (#120)

---
+++ src/test/java/com/team1/otvoo/OtvooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.otvoo;
+
+import com.team1.otvoo.common.AbstractPostgresTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtvooApplicationTests extends AbstractPostgresTest {
+
+  @Test
+  void contextLoads() {
+  }
+}
